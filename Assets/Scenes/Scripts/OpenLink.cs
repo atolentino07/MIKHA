6f951ca0e7f5480feddf201c67a967f95c88be60
atolentino07 <102979320+atolentino07@users.noreply.github.com>
2024-11-13 14:30:13 -0800

    Download button updated

--- Assets/Scenes/Scripts/OpenLink.cs
+++ Assets/Scenes/Scripts/OpenLink.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class OpenLink : MonoBehaviour
+public class OpenLinkButton : MonoBehaviour
 {
     // Link to your Google Drive file or folder
     public string googleDriveUrl = "https://drive.google.com/drive/folders/15XN9GY61dyiZMCu7anOhqBsc_iKiKG9z?usp=sharing";
